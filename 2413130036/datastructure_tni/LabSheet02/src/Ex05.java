7b4024923cf934c1b2b0d54be5d48a45ab60ed2e
2413130036-cyber <2413130036@tni.ac.th>
2026-07-16 16:45:48 +0700

    lol

---
+++ 2413130036/datastructure_tni/LabSheet02/src/Ex05.java
@@ -0,0 +1,22 @@
+
+public class Ex05 {
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+    NumberArrayController nums = new NumberArrayController();
+    nums.display("Display an initial array:");
+    
+	int[] new_nums = new int[nums.getNums().length-1];
+	
+	for (int i=0; i<new_nums.length; i++) {
+		new_nums[i] = nums.getNums()[i+1];
+	}
+	
+	
+	nums.setNums(new_nums);
+	nums.display("\nAfter deleted the first element:");
+	
+	
+     }
+
+}
---
+++ 2413130036/datastructure_tni/LabSheet03/src/NumberLinkedList.java
@@ -0,0 +1,15 @@
+
+public class NumberLinkedList {
+ 
+	public static void main(String[] args) {
+		LinkedLlist numbers = new LinkedLlist();
+		numbers.insert(0, 37);
+		numbers.insert(0, 7);
+		numbers.insert(0, 4);
+		numbers.insert(0, 16);
+		
+		System.out.println("All elements = " + numbers.traversal());
+ 
+	}
+ 
+}
